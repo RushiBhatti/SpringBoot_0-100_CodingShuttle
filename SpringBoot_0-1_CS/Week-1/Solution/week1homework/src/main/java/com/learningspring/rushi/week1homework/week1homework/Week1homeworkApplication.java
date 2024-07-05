c2e064c226fb4a777391e1c74a87fcebbbed3c73
Rushi Bhatti <139007370+RushiBhatti@users.noreply.github.com>
2024-07-06 01:08:19 +0530

    add question for refference

--- SpringBoot_0-1_CS/Week-1/Solution/week1homework/src/main/java/com/learningspring/rushi/week1homework/week1homework/Week1homeworkApplication.java
+++ SpringBoot_0-1_CS/Week-1/Solution/week1homework/src/main/java/com/learningspring/rushi/week1homework/week1homework/Week1homeworkApplication.java
@@ -12,6 +12,22 @@ public class Week1homeworkApplication implements CommandLineRunner {
 	CakeBaker cakeBaker;
 
 	public static void main(String[] args) {
+
+		/*
+		HOMEWORK QUESTION: Alice and her Bakery
+		
+		- Create a class called CakeBaker, that is dependent on two other
+		classes called Frosting and Syrup. This class has a function
+		called bakeCake().
+		- Create two interfaces of type Frosting and Syrup with a
+		function called getFrostingType and getSyrupType respectively.
+		- Create two implementations of these two interfaces (so total 4
+		classes) for Chocolate and Strawberry flavors.
+		- Use Dependency injection to inject the Frosting and Syrup
+		dependencies into CakeBaker and also to call the bakeCake
+		function of the CakeBaker class
+		*/
+
 		SpringApplication.run(Week1homeworkApplication.class, args);
 	}
 
